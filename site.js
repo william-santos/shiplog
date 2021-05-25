4024ab32da6d6f54e8bcbec2f5a6fcaea3f9fe50
William <wr.sant@gmail.com>
2021-05-25 08:06:37 -0300

    Nova proposta de página

--- site.js
+++ site.js
@@ -6,8 +6,10 @@ if (hamburger) {
   hamburger.addEventListener('click', (e) => {
     if (menu.classList.contains('hidden')) {
       menu.classList.remove('hidden');
+      menu.classList.add('is-open');
     } else {
       menu.classList.add('hidden');
+      menu.classList.remove('is-open');
     }
   });
 }
@@ -20,6 +22,12 @@ for (const link of links) {
 
 function clickHandler(e) {
   e.preventDefault();
+
+  if (menu.classList.contains('is-open')) {
+    menu.classList.add('hidden');
+    menu.classList.remove('is-open');
+  }
+
   const href = this.getAttribute("href");
   const offsetTop = document.querySelector(href).offsetTop;
   
